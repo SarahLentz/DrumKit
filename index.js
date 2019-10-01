1fabead4f872b84d7555910d714262434c1e4a8b
SarahLentz <54770809+SarahLentz@users.noreply.github.com>
2019-10-01 12:55:58 -0500

    Removed "sounds/" from file paths of sound files.

--- index.js
+++ index.js
@@ -25,27 +25,27 @@ function makeSound(key) {   // function(<function name>(input parameter))
       audio.play();
     break;
     case "a":
-      audio = new Audio('sounds/tom-2.mp3');
+      audio = new Audio('tom-2.mp3');
       audio.play();
     break;
     case "s":
-      audio = new Audio('sounds/snare.mp3');
+      audio = new Audio('snare.mp3');
       audio.play();
     break;
     case "d":
-      audio = new Audio('sounds/tom-3.mp3');
+      audio = new Audio('tom-3.mp3');
       audio.play();
     break;
     case "j":
-      audio = new Audio('sounds/tom-4.mp3');
+      audio = new Audio('tom-4.mp3');
       audio.play();
     break;
     case "k":
-      audio = new Audio('sounds/kick-bass.mp3');
+      audio = new Audio('kick-bass.mp3');
       audio.play();
     break;
     case "l":
-      audio = new Audio('sounds/crash.mp3');
+      audio = new Audio('crash.mp3');
       audio.play();
     break;
     default:
